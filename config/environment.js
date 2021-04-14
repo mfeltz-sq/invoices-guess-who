43a535150884678866edceaf12c706757ae3104c
Margaret Feltz <mfeltz@squareup.com>
2021-04-14 16:03:19 -0700

    ember-cli-github-pages

--- config/environment.js
+++ config/environment.js
@@ -44,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV.rootURL = '/invoices-guess-who/'
+    // here you can enable a production-specific feature
   }
 
   return ENV;

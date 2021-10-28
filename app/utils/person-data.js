cae69861f81f30dc1c57722d38a6d537d5d9e642
Margaret Feltz <mfeltz@squareup.com>
2021-10-28 11:49:32 -0700

    add people

--- app/utils/person-data.js
+++ app/utils/person-data.js
@@ -6,13 +6,17 @@ export default [
     img: `${config.rootURL}assets/images/aaron.jpg`
   },
   {
+    name: 'anthony',
+    img: `${config.rootURL}assets/images/anthony.png`
+  },
+  {
     name: 'austin',
     img: `${config.rootURL}assets/images/austin.png`
   },
-  {
-    name: 'ben',
-    img: `${config.rootURL}assets/images/ben.jpg`
-  },
+  // {
+  //   name: 'ben',
+  //   img: `${config.rootURL}assets/images/ben.jpg`
+  // },
   {
     name: 'brad',
     img: `${config.rootURL}assets/images/brad.jpg`
@@ -22,16 +26,28 @@ export default [
     img: `${config.rootURL}assets/images/carrie.jpg`
   },
   {
+    name: 'craig',
+    img: `${config.rootURL}assets/images/craig.png`
+  },
+  {
     name: 'efe',
     img: `${config.rootURL}assets/images/efe.jpg`
+  },
+  {
+    name: 'elan',
+    img: `${config.rootURL}assets/images/elan.png`
   },
   {
     name: 'hung',
     img: `${config.rootURL}assets/images/hung.jpg`
   },
+  // {
+  //   name: 'ini',
+  //   img: `${config.rootURL}assets/images/ini.jpg`
+  // },
   {
-    name: 'ini',
-    img: `${config.rootURL}assets/images/ini.jpg`
+    name: 'jae',
+    img: `${config.rootURL}assets/images/jae.png`
   },
   {
     name: 'jen',
@@ -46,20 +62,44 @@ export default [
     img: `${config.rootURL}assets/images/jesse.png`
   },
   {
+    name: 'judy',
+    img: `${config.rootURL}assets/images/judy.png`
+  },
+  {
     name: 'kaitlyn',
     img: `${config.rootURL}assets/images/kaitlyn.jpg`
+  },
+  {
+    name: 'kushan',
+    img: `${config.rootURL}assets/images/kushan.png`
+  },
+  {
+    name: 'kyle',
+    img: `${config.rootURL}assets/images/kyle.png`
+  },
+  {
+    name: 'lesley',
+    img: `${config.rootURL}assets/images/lesley.png`
   },
   {
     name: 'margaret',
     img: `${config.rootURL}assets/images/margaret.jpg`
   },
   {
-    name: 'matt',
-    img: `${config.rootURL}assets/images/matt.jpg`
+    name: 'matt b',
+    img: `${config.rootURL}assets/images/mattb.jpg`
+  },
+  {
+    name: 'matt d',
+    img: `${config.rootURL}assets/images/mattd.png`
   },
   {
     name: 'maya',
     img: `${config.rootURL}assets/images/maya.jpg`
+  },
+  {
+    name: 'mengqi',
+    img: `${config.rootURL}assets/images/mengqi.png`
   },
   {
     name: 'michael',
@@ -69,9 +109,13 @@ export default [
     name: 'monica',
     img: `${config.rootURL}assets/images/monica.png`
   },
+  // {
+  //   name: 'nik',
+  //   img: `${config.rootURL}assets/images/nik.png`
+  // },
   {
-    name: 'nik',
-    img: `${config.rootURL}assets/images/nik.png`
+    name: 'olivia',
+    img: `${config.rootURL}assets/images/olivia.png`
   },
   {
     name: 'paul',
@@ -82,13 +126,17 @@ export default [
     img: `${config.rootURL}assets/images/ramin.png`
   },
   {
-    name: 'ryan',
-    img: `${config.rootURL}assets/images/ryan.jpg`
+    name: 'ryan l',
+    img: `${config.rootURL}assets/images/ryanl.jpg`
   },
   {
-    name: 'sam',
-    img: `${config.rootURL}assets/images/sam.png`
+    name: 'ryan s',
+    img: `${config.rootURL}assets/images/ryans.png`
   },
+  // {
+  //   name: 'sam',
+  //   img: `${config.rootURL}assets/images/sam.png`
+  // },
   {
     name: 'sudheer',
     img: `${config.rootURL}assets/images/sudheer.jpg`
